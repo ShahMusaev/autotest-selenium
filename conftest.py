4893216d76fe87b2ca82cf0657d51d00a9f034e7
ShahMusaev <shahrommusaev@gmail.com>
2021-11-14 15:16:34 +0300

    add default english language

--- conftest.py
+++ conftest.py
@@ -2,8 +2,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="Choose browser language")
 
 
@@ -13,6 +14,6 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(25.0)
+    browser.implicitly_wait(5.0)
     yield browser
     browser.quit()
