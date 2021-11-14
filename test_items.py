ec33844c35942743f65d8eae42aee88ddd08db77
ShahMusaev <shahrommusaev@gmail.com>
2021-11-14 14:54:24 +0300

    add test for check available add to cart button

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+
+def test_available_add_to_cart_button(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(30.0)
+    add_2_cart = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert add_2_cart is not None, 'Add to Cart button not found'
